aebb3f531cf01ebd5d3ff366ce79f09e5ce0c040
Prathmesh Desai <142252602+pd241008@users.noreply.github.com>
2025-08-03 13:56:47 +0530

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -46,7 +46,7 @@ export default async function DashboardRoute() {
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {data.map((item) => (
-          <BlogPostCard data={item} key={item.id} alt="image" />
+          <BlogPostCard data={item} key={item.id} />
         ))}
       </div>
     </div>
